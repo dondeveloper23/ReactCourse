9d6aa3bde842485704c32bbe9c27bba96bde0cd1
ducce3 <sasicdusan4@gmail.com>
2026-02-19 02:54:51 +0100

    Revert "lecture 7 initial commit"
    This reverts commit e3ad47afcf217bd93d4cbba771c51279e89713c7.

--- search-project/src/Components/AllVideos.js
+++ search-project/src/Components/AllVideos.js
@@ -1,8 +1,9 @@
 
+import VIDEOS from "./../videos.json"
 
-const GetAllVideos = ({videos}) => {
+const GetAllVideos = () => {
     return (
-        videos.map( video => {
+        VIDEOS.map( video => {
                 return <a href={video.url} key={video.id} target="_blank" rel="noopener noreferrer">
                     <img src={video.cover} alt={video.title} />
                     <h3>{video.title}</h3>
--- search-project/src/Pages/Home.js
+++ search-project/src/Pages/Home.js
@@ -1,22 +1,9 @@
 import AllVideos from "../Components/AllVideos";
-import SearchBar from "../Components/SearchBar";
-import {useState} from "react";
-import VIDEOS from "./../videos.json"
 
 const Home = () => {
-
-    const [videos, setVideos] = useState(VIDEOS);
-    const [filteredVideos, setFilteredVideos] = useState(VIDEOS);
-
-    const handleSearchVideos = (results) => {
-      setFilteredVideos(results)
-    };
-
-
     return (
         <>
-            <SearchBar  videos={videos} onSearchVideo={handleSearchVideos}/>
-            <AllVideos videos={filteredVideos}/>
+            <AllVideos />
         </>
     )
 }
